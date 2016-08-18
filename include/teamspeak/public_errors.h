068e4fae0a77bd37c6a7f4d034742e429a29a2f3
Marius <git@mgraefe.de>
2016-08-18 18:09:32 +0200

    New plugin api 21

--- include/teamspeak/public_errors.h
+++ include/teamspeak/public_errors.h
@@ -15,6 +15,7 @@ const unsigned int ERROR_lib_time_limit_reached                 = 0x0005;
 const unsigned int ERROR_command_not_found                      = 0x0100;
 const unsigned int ERROR_unable_to_bind_network_port            = 0x0101;
 const unsigned int ERROR_no_network_port_available              = 0x0102;
+const unsigned int ERROR_port_already_in_use                    = 0x0103;
 
 //client
 const unsigned int ERROR_client_invalid_id                      = 0x0200;
@@ -85,6 +86,33 @@ const unsigned int ERROR_clientlibrary_not_initialised          = 0x0709;
 const unsigned int ERROR_serverlibrary_not_initialised          = 0x070a;
 const unsigned int ERROR_whisper_too_many_targets               = 0x070b;
 const unsigned int ERROR_whisper_no_targets                     = 0x070c;
+const unsigned int ERROR_connection_ip_protocol_missing         = 0x070d;
+
+//file transfer
+const unsigned int ERROR_file_invalid_name                      = 0x0800;
+const unsigned int ERROR_file_invalid_permissions               = 0x0801;
+const unsigned int ERROR_file_already_exists                    = 0x0802;
+const unsigned int ERROR_file_not_found                         = 0x0803;
+const unsigned int ERROR_file_io_error                          = 0x0804;
+const unsigned int ERROR_file_invalid_transfer_id               = 0x0805;
+const unsigned int ERROR_file_invalid_path                      = 0x0806;
+const unsigned int ERROR_file_no_files_available                = 0x0807;
+const unsigned int ERROR_file_overwrite_excludes_resume         = 0x0808;
+const unsigned int ERROR_file_invalid_size                      = 0x0809;
+const unsigned int ERROR_file_already_in_use                    = 0x080a;
+const unsigned int ERROR_file_could_not_open_connection         = 0x080b;
+const unsigned int ERROR_file_no_space_left_on_device           = 0x080c;
+const unsigned int ERROR_file_exceeds_file_system_maximum_size  = 0x080d;
+const unsigned int ERROR_file_transfer_connection_timeout       = 0x080e;
+const unsigned int ERROR_file_connection_lost                   = 0x080f;
+const unsigned int ERROR_file_exceeds_supplied_size             = 0x0810;
+const unsigned int ERROR_file_transfer_complete                 = 0x0811;
+const unsigned int ERROR_file_transfer_canceled                 = 0x0812;
+const unsigned int ERROR_file_transfer_interrupted              = 0x0813;
+const unsigned int ERROR_file_transfer_server_quota_exceeded    = 0x0814;
+const unsigned int ERROR_file_transfer_client_quota_exceeded    = 0x0815;
+const unsigned int ERROR_file_transfer_reset                    = 0x0816;
+const unsigned int ERROR_file_transfer_limit_reached            = 0x0817;
 
 //sound
 const unsigned int ERROR_sound_preprocessor_disabled            = 0x0900;
