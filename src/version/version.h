f149121f1066f0613a160e89d696292c961a760b
Marius Gräfe <git@mgraefe.de>
2016-08-21 18:33:46 +0200

    Updater can now display a changelog

---
+++ src/version/version.h
@@ -0,0 +1,13 @@
+//CHANGES WILL BE OVERWRITTEN BY version.py
+
+#ifndef ts3sb_all__version_H__
+#define ts3sb_all__version_H__
+
+#define TS3SB_VERSION 1,2,5,1424
+#define TS3SB_VERSION_S "1.2.5.1424"
+#define TS3SB_VERSION_MAJOR 1
+#define TS3SB_VERSION_MINOR 2
+#define TS3SB_VERSION_REVISION 5
+#define TS3SB_VERSION_BUILD 1424
+
+#endif
