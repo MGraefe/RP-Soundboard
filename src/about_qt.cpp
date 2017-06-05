ae3f073d15b551a77050c0b601676fbeb1da9115
Marius <git@mgraefe.de>
2017-06-05 15:54:46 +0200

    Added ffmpeg version info to about window

--- src/about_qt.cpp
+++ src/about_qt.cpp
@@ -10,6 +10,12 @@
 #include "about_qt.h"
 #include "buildinfo.h"
 
+extern "C"
+{
+#include <libavutil\avutil.h>
+}
+
+
 //---------------------------------------------------------------
 // Purpose: 
 //---------------------------------------------------------------
@@ -17,8 +23,10 @@ AboutQt::AboutQt(QWidget *parent) :
 	QWidget(parent, Qt::Window | Qt::WindowTitleHint /*| Qt::CustomizeWindowHint*/ | Qt::WindowCloseButtonHint),
 	ui(new Ui::AboutQt)
 {
+	const char *ffmpeg_version = av_version_info();
 	ui->setupUi(this);
 	ui->l_version->setText(QString(buildinfo_getPluginVersion()) + 
-		"\nBuild on " + buildinfo_getBuildDate() + " " + buildinfo_getBuildTime());
+		"\nBuild on " + buildinfo_getBuildDate() + " " + buildinfo_getBuildTime() +
+	    "\nFFmpeg Version: " + (ffmpeg_version ? ffmpeg_version : "unknown"));
 	setFixedSize(size());
 }
