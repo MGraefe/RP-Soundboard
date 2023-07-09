9d8c2d30fc386d06f97feec783d3d64be342e1a6
Marius Gräfe <git@mgraefe.de>
2023-07-09 16:54:34 +0200

    Cleanup of old release folder

--- release/create.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-copy ..\build32\release\rp_soundboard_win32.dll plugins\
-copy ..\build64\release\rp_soundboard_win64.dll plugins\
-set /p build="Build number: "
-mkdir releases\%build%
-"C:\Program Files\7-Zip\7z.exe" a releases\%build%\rp_soundboard_%build%.ts3_plugin -tzip -mx=9 -mm=Deflate ./package.ini plugins/*
-pause
