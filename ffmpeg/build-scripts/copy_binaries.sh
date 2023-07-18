360d7188c987f6d2518e8271814b91da53e840eb
Marius Gräfe <git@mgraefe.de>
2023-07-18 17:46:20 +0200

    testing linux build action

--- ffmpeg/build-scripts/copy_binaries.sh
+++ ffmpeg/build-scripts/copy_binaries.sh
@@ -23,17 +23,22 @@ esac
 
 pushd ..
 
-mkdir -p "${prefix}_x64"
-mkdir -p "${prefix}_x86"
+if [ -d "ffmpeg/x64" ]; then
+	mkdir -p "${prefix}_x64"
+	cp ffmpeg/x64/lib/libavcodec.a ${prefix}_x64/${infix}avcodec.$postfix
+	cp ffmpeg/x64/lib/libavformat.a ${prefix}_x64/${infix}avformat.$postfix
+	cp ffmpeg/x64/lib/libavutil.a ${prefix}_x64/${infix}avutil.$postfix
+	cp ffmpeg/x64/lib/libswresample.a ${prefix}_x64/${infix}swresample.$postfix
+	cp -R ffmpeg/x64/include ${prefix}_x64/include
+fi
 
-cp ffmpeg/x64/lib/libavcodec.a ${prefix}_x64/${infix}avcodec.$postfix
-cp ffmpeg/x64/lib/libavformat.a ${prefix}_x64/${infix}avformat.$postfix
-cp ffmpeg/x64/lib/libavutil.a ${prefix}_x64/${infix}avutil.$postfix
-cp ffmpeg/x64/lib/libswresample.a ${prefix}_x64/${infix}swresample.$postfix
-cp -R ffmpeg/x64/include ${prefix}_x64/include
+if [ -d "ffmpeg/x86" ]; then
+	mkdir -p "${prefix}_x86"
+	cp ffmpeg/x86/lib/libavcodec.a ${prefix}_x86/${infix}avcodec.$postfix
+	cp ffmpeg/x86/lib/libavformat.a ${prefix}_x86/${infix}avformat.$postfix
+	cp ffmpeg/x86/lib/libavutil.a ${prefix}_x86/${infix}avutil.$postfix
+	cp ffmpeg/x86/lib/libswresample.a ${prefix}_x86/${infix}swresample.$postfix
+	cp -R ffmpeg/x86/include ${prefix}_x86/include
+fi
 
-cp ffmpeg/x86/lib/libavcodec.a ${prefix}_x86/${infix}avcodec.$postfix
-cp ffmpeg/x86/lib/libavformat.a ${prefix}_x86/${infix}avformat.$postfix
-cp ffmpeg/x86/lib/libavutil.a ${prefix}_x86/${infix}avutil.$postfix
-cp ffmpeg/x86/lib/libswresample.a ${prefix}_x86/${infix}swresample.$postfix
-cp -R ffmpeg/x86/include ${prefix}_x86/include
+popd
